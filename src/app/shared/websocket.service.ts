0596a88f67cf21f706cb7768fb6a5178c80613b6
Olivier Tilmans <olivier.tilmans@uclouvain.be>
2017-06-06 21:58:19 +0200

    Update websocket service to work with phoenix
    Signed-off-by: Olivier Tilmans <olivier.tilmans@uclouvain.be>

--- src/app/shared/websocket.service.ts
+++ src/app/shared/websocket.service.ts
@@ -1,36 +1,88 @@
-import {
-  Injectable
-} from '@angular/core';
-import { Subject, Observer, Observable } from 'rxjs/Rx';
+import { Injectable, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import { Socket, Channel } from 'phoenix';
+import 'rxjs/add/operator/map';
 
-@Injectable()
-export class WebsocketService {
-  private socket: Subject < MessageEvent > ;
+import { Logger } from './logger.service';
 
-  public connect(url): Subject < MessageEvent > {
-    if (!this.socket) {
-      this.socket = this.create(url);
-    }
-    return this.socket;
+
+/* FIXME remap dynamically hostname/port */
+const API_ENDPOINT = `ws://localhost:4000/socket`;
+
+
+export class WebSocketChannel {
+  private channel: Channel;
+
+  constructor(private socket: Socket, public readonly topic: string,
+              private log: Logger, options = {}) {
+    this.channel = socket.channel(topic, options);
   }
 
-  private create(url): Subject < MessageEvent > {
-    let ws = new WebSocket(url);
-    let observable = Observable.create(
-      (obs: Observer < MessageEvent > ) => {
-        ws.onmessage = obs.next.bind(obs);
-        ws.onerror = obs.error.bind(obs);
-        ws.onclose = obs.complete.bind(obs);
-        return ws.close.bind(ws);
-      }
-    );
-    let observer = {
-      next: (data: Object) => {
-        if (ws.readyState === WebSocket.OPEN) {
-          ws.send(JSON.stringify(data));
-        }
-      },
-    };
-    return Subject.create(observer, observable);
+  /* Join the channel and start receiving events (or push them) */
+  public join(options = {}) {
+    this.channel.join()
+      .receive("ok", resp => this.log.log(`Connected to ${resp}`))
+      .receive("error", resp => this.log.error(`Unable to join ${resp}`));
+  }
+
+  /* Subscribe to events in the channel named 'key',
+  * and parse their associated meaning */
+  public subscribe<T>(key) : Observable<T> {
+    return new Observable(o => {
+      this.channel.on(key, resp => {
+        this.log.log(`New message for ${this.topic}[${key}]: ${resp}`);
+        o.next(resp); });
+    }).map(resp => resp as T);
+  }
+
+  /* Execute the given function when an event is received in the channel */
+  public on(key, f) {
+    this.channel.on(key, f);
+  }
+
+  /* Push a message to the channel, optionally with a payload */
+  public push(key, val = {}) {
+    this.channel.push(key, val);
+  }
+
+  /* Close the underlying socket channel, both on the client and the server */
+  public leave() {
+    if (!this.isActive()) {
+        this.log.warn(`Tried to leave ${this.topic}
+                      in state: ${this.channel.state}`);
+        return;
+    }
+    this.channel.leave()
+      .receive("ok", () => console.log(`Left channel ${ this.topic }`));
+  }
+
+  /* Return whether this channel is joined/joining and thus able to be used */
+  public isActive() {
+    return this.channel.isJoined() || this.channel.isJoining();
+  }
+}
+
+
+@Injectable()
+export class WebsocketService implements OnDestroy {
+  private socket: Socket = new Socket(API_ENDPOINT);
+  private _channels: Channel[] = [];
+
+  constructor(private log: Logger) {
+    this.log = this.log.getLogger("Websocket");
+    this.socket.connect();
+  }
+
+  public channel(topic, options = {}) {
+    let chan = new WebSocketChannel(this.socket, topic, this.log, options);
+    this._channels.push(chan);
+    chan.join();
+    return chan;
+  }
+
+  ngOnDestroy() {
+    this.log.log('Clossing websocket');
+    for (let chan of this._channels)
+          chan.leave()
   }
 }
