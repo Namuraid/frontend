cc9c0dc097edbb652348f520a28353221d534fff
Olivier Tilmans <olivier.tilmans@uclouvain.be>
2017-06-25 13:41:56 +0200

    fix videofeed race condition

--- src/app/views/videofeed-view.component.ts
+++ src/app/views/videofeed-view.component.ts
@@ -21,34 +21,37 @@ export class VideofeedViewComponent implements OnInit{
   private screenWidth: number;
   private screenHeight: number;
   private _route_subscription = null;
-  private screenName = 'test'
+  private screenName;
   feed$: Observable<string>;
-  style = ''
+  style = '';
 
   constructor(private ws: WebsocketService, private http: Http,
               private sanitizer: DomSanitizer,
               private screen: ScreenService,
               private route: ActivatedRoute) {
-    this.chan = this.ws.channel('livefeed');
-
-    this.channel = this.ws.channel(`screen:${this.screenName}`);
-    this.channel.join_noreturn()
-      .receive("ok", ({style}) => {
-      console.log("screen");
-
-      this.screenWidth = style["width.px"]
-      this.screenHeight = style["height.px"]
-
-      const subscription = this.chan.join()
-      .subscribe(({ token, pageId, style }) => {
-        this.loadFeed(token, pageId, subscription, style) })
-
-    });
   }
 
   ngOnInit() {
     this._route_subscription = this.route.params.subscribe(
-      p => this.screenName = p['id']);
+      p => {
+        this.screenName = p['id']
+
+        this.chan = this.ws.channel('livefeed');
+
+        this.channel = this.ws.channel(`screen:${this.screenName}`);
+        this.channel.join_noreturn()
+          .receive("ok", ({style}) => {
+          console.log("screen");
+
+          this.screenWidth = style["width.px"]
+          this.screenHeight = style["height.px"]
+
+          const subscription = this.chan.join()
+          .subscribe(({ token, pageId, style }) => {
+            this.loadFeed(token, pageId, subscription, style) })
+
+        });
+      });
   }
 
   loadFeed(token: string, pageId: string, subscription, style: string) {
