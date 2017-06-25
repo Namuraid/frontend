e00427eba42407a4149009caa4a4a89a4ff689ea
Simon Claessens <gagalago@gmail.com>
2017-06-25 10:01:39 +0200

    fb

--- src/app/views/videofeed-view.component.ts
+++ src/app/views/videofeed-view.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ScreenService } from './../shared/screen.service';
+import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs';
 import { DomSanitizer } from '@angular/platform-browser';
@@ -13,22 +15,59 @@ import {
     templateUrl: './videofeed-view.component.html',
     styleUrls: ['./videofeed-view.component.css']
 })
-export class VideofeedViewComponent {
+export class VideofeedViewComponent implements OnInit{
   private chan: WebSocketChannel;
+  private channel: WebSocketChannel;
+  private screenWidth: number;
+  private screenHeight: number;
+  private _route_subscription = null;
+  private screenName = 'test'
   feed$: Observable<string>;
+  style = ''
 
   constructor(private ws: WebsocketService, private http: Http,
-              private sanitizer: DomSanitizer) {
+              private sanitizer: DomSanitizer,
+              private screen: ScreenService,
+              private route: ActivatedRoute) {
     this.chan = this.ws.channel('livefeed');
-    const subscription = this.chan.join().take(1)
-      .subscribe(({ token, pageId }) => {
-        this.loadFeed(token, pageId, subscription) })
+
+    this.channel = this.ws.channel(`screen:${this.screenName}`);
+    this.channel.join_noreturn()
+      .receive("ok", ({style}) => {
+      console.log("screen");
+
+      this.screenWidth = style["width.px"]
+      this.screenHeight = style["height.px"]
+
+      const subscription = this.chan.join()
+      .subscribe(({ token, pageId, style }) => {
+        this.loadFeed(token, pageId, subscription, style) })
+
+    });
   }
 
-  loadFeed(token: string, pageId: string, subscription) {
+  ngOnInit() {
+    this._route_subscription = this.route.params.subscribe(
+      p => this.screenName = p['id']);
+  }
+
+  loadFeed(token: string, pageId: string, subscription, style: string) {
+    this.style = style
+
     this.feed$ = this.http.get(`https://graph.facebook.com/v2.9/${pageId}?fields=live_videos.limit(1).order(chronological)%7Bembed_html%7D&access_token=${token}`)
-      .map((res) => this.sanitizer.bypassSecurityTrustHtml(
-        res.json().live_videos.data[0].embed_html))
+      .map((res) => {
+
+        let embed:string = res.json().live_videos.data[0].embed_html;
+        const heightRatio = this.screenHeight / 640
+        const widthRatio = this.screenWidth / 360
+        const ratio = Math.min(heightRatio, widthRatio)
+        console.log(this);
+        const width = 360 * ratio
+        const height = 100000//this.screenHeight / ratio
+        embed = embed.replace(`width="360"`, `width="${width}"`)
+        embed = embed.replace(`height="640"`, `height="${height}"`)
+        return this.sanitizer.bypassSecurityTrustHtml(embed);
+      })
     subscription.unsubscribe()
   }
 }
